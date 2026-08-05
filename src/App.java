501c5a55b8b83e93bc5d10b9153110a3a54debda
CorMcD317 <mcdoucor000@warren.k12.in.us>
2026-08-05 13:56:24 -0400

    First Commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String myName = "McDougal";
+        System.out.println(myName);
+
+        myName = "Corbin";
+        System.out.println(myName);
+
+        char MyLetter = 'C';
+
+    }
+}
+
