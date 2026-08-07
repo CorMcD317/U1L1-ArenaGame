86a843f4e5ef59a41aca08c352540d30924e3f36
CorMcD317 <mcdoucor000@warren.k12.in.us>
2026-08-07 13:44:09 -0400

    Commit

--- src/App.java
+++ src/App.java
@@ -1,12 +1,24 @@
 public class App {
+
+    static final int MAX_HEALTH = 100;
+
+    static final int STARTING_GOLD = 20;
+
     public static void main(String[] args) throws Exception {
-        String myName = "McDougal";
-        System.out.println(myName);
 
-        myName = "Corbin";
-        System.out.println(myName);
+        String playerName = "Player1";
+        
+        int health = MAX_HEALTH;
 
-        char MyLetter = 'C';
+        boolean isAlive = true;
+
+        double xritChance = 0.15;
+
+        int gold = STARTING_GOLD;
+
+        //TODO: make a String called enemyName, assin you choice
+        //TODO: enemyHealth of some int between 0 and 50
+        //TODO: enemyPower of some int between 0 and 20
 
     }
 }
